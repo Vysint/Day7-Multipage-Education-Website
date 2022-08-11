33e0b6c0e7557caa4af88bbbf73d668a49c0a57d
Vincent Kipngetich <vinniebluz@icloud.com>
2022-08-12 02:07:38 +0300

    Added Javascript click event handler to toggle the Paragraph in the FAQS section

--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,23 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0);
 })
+
+
+// show/hide the faqs
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = "uil uil-minus";
+        }else{
+            icon.className = "uil uil-plus";
+        }
+    })
+})
